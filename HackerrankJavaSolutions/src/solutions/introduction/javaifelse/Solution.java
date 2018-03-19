d2e5330085edc2849410fecd90273d91f18f106b
ShankarR <ShankarR@NPF0SKJU9.sqs.group.intl>
2018-03-19 13:29:16 +0530

    Committing Hacker Rank Solutions

---
+++ HackerrankJavaSolutions/src/solutions/introduction/javaifelse/Solution.java
@@ -0,0 +1,19 @@
+package solutions.introduction.javaifelse;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();            
+        String ans="";
+        
+        if(n%2!=0) ans = "Weird";
+        else if (n>=2 && n<=5) ans = "Not Weird";
+        else if (n>=6 && n<=20) ans = "Weird";
+        else if (n>20) ans = "Not Weird";
+        System.out.println(ans);
+	}
+
+}
