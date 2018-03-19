046a6b3bb637e4ccf55bdd3f740744434890dfdf
ShankarR <ShankarR@NPF0SKJU9.sqs.group.intl>
2018-03-19 15:47:34 +0530

    Committed Additional Solutions 1) Java Loops 1 2) Java Loops 2 3) Java Output Format 4) Java Stdin Out 1 5) Java StdinOut 2

--- HackerrankJavaSolutions/src/solutions/introduction/javastdinout2/Solution.java
+++ HackerrankJavaSolutions/src/solutions/introduction/javastdinout2/Solution.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class Solution {
 
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        scan.nextLine();
-        String s = scan.nextLine();
-
-        // Write your code here.
-        
-
-        System.out.println("String: " + s);
-        System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
-        scan.close();
+		Scanner sc=new Scanner(System.in);
+        int q = sc.nextInt();
+        while(sc.hasNext())
+        {
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
+        	int n = sc.nextInt();
+        	int sum = a;
+        	for(int i=0; i<n; i++)
+        	{
+        		sum += (int)((Math.pow(2, i))*b);
+        		System.out.print(sum+" ");        		
+        	}
+        	System.out.println();
+        }
+        sc.close();
 	}
 
 }
