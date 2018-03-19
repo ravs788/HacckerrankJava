6e55c3b6a0e059a7fd87413df61161755d41fe7e
ShankarR <ShankarR@NPF0SKJU9.sqs.group.intl>
2018-03-19 13:28:50 +0530

    Committed Hacker Rank Solutions

---
+++ HackerrankJavaSolutions/src/solutions/introduction/welcometojava/Solution.java
@@ -0,0 +1,10 @@
+package solutions.introduction.welcometojava;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+
+}
